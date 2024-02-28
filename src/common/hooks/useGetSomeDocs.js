33e02b80fc2a8f8ae616fb459ed70a2b6ae53c84
alexfedd <alexandrfedorov508@gmail.com>
2024-02-28 20:12:43 +0300

    Bad

--- src/common/hooks/useGetSomeDocs.js
+++ src/common/hooks/useGetSomeDocs.js
@@ -1,22 +1,34 @@
-import { useQuery, keepPreviousData } from '@tanstack/react-query';
-import { collection, getDocs, limit, orderBy, query } from 'firebase/firestore';
+import { useQuery, keepPreviousData, useInfiniteQuery } from '@tanstack/react-query';
+import { collection, getDocs, limit, orderBy, query, startAfter } from 'firebase/firestore';
 import { db } from '../../setup/auth';
 
 export function useGetSomeDocs(numberOfItems, bdKey) {
-    return useQuery({
+    return useInfiniteQuery({
         queryKey: [bdKey, numberOfItems],
-        queryFn: async () => {
+        queryFn: async (pageParam = 1) => {
             const collectionRef = collection(db, bdKey);
             let q;
             if(bdKey === 'posts') {
-                q = query(collectionRef, limit(numberOfItems), orderBy('date', 'desc'))
+                q = query(collectionRef,orderBy('date', 'desc'), limit(numberOfItems), startAfter(pageParam))
             }
             else {
-                q = query(collectionRef, limit(numberOfItems))
+                q = query(collectionRef, limit(numberOfItems), startAfter(pageParam))
             }
             const querySnapshot = await getDocs(q);
             const users = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
             return users;
+        },
+        getNextPageParam: (lastPage, _, lastPageParam) => {
+            if (lastPage.length === 0) {
+                return undefined;
+            }
+            return lastPageParam + 1
+        },
+        getPreviousPageParam: (_, __, firstPageParam) => {
+            if (firstPageParam <= 1) {
+                return undefined;
+            }
+            return firstPageParam - 1;
         },
         placeholderData: keepPreviousData,
     });
--- src/pages/layout/components/rightBar/RightBar.jsx
+++ src/pages/layout/components/rightBar/RightBar.jsx
@@ -3,9 +3,10 @@ import { useSelector } from "react-redux";
 import UserCard from "./components/UserName/UserCard";
 import "./style.scss";
 import { useGetSomeDocs } from "../../../../common/hooks/useGetSomeDocs";
+import { Fragment } from "react";
 function RightBar() {
   const USER_AMOUNT = 8;
-  const { data: usersData, isLoading } = useGetSomeDocs(USER_AMOUNT, 'users');
+  const { data: usersData, isLoading } = useGetSomeDocs(USER_AMOUNT, "users");
   const { currentUserID } = useSelector((state) => state.auth);
   if (isLoading) {
     return <p>Loading...</p>;
@@ -15,20 +16,25 @@ function RightBar() {
       <div className="right-bar__users">
         <h2 className="title-h2">Other users</h2>
         <div className="right-bar__users-list">
-          {usersData.map((user) => {
-            if (user.id === currentUserID) {
-              return;
-            }
-            return (
-              <UserCard
-                key={user.id}
-                user={user}
-                currentUserFollowing={
-                  usersData.find((item) => item.id === currentUserID)?.following
+          {usersData?.pages.map((group, index) => {
+            <Fragment key={index}>
+              {group.map((user) => {
+                if (user.id === currentUserID) {
+                  return;
                 }
-                userAmount={USER_AMOUNT}
-              />
-            );
+                return (
+                  <UserCard
+                    key={user.id}
+                    user={user}
+                    currentUserFollowing={
+                      usersData.find((item) => item.id === currentUserID)
+                        ?.following
+                    }
+                    userAmount={USER_AMOUNT}
+                  />
+                );
+              })}
+            </Fragment>;
           })}
         </div>
       </div>
