b3c2387648a4672344830164fd19b1a9943c4137
alexfedd <alexandrfedorov508@gmail.com>
2024-02-28 19:35:46 +0300

    Revert "Bad"
    This reverts commit 669955539672f1438227687625a96e0f1597ba58.

--- src/common/hooks/useGetSomeDocs.js
+++ src/common/hooks/useGetSomeDocs.js
@@ -1,24 +1,23 @@
-import { keepPreviousData, useInfiniteQuery  } from '@tanstack/react-query';
-import { collection, getDocs, limit, orderBy, query, startAfter } from 'firebase/firestore';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import { collection, getDocs, limit, orderBy, query } from 'firebase/firestore';
 import { db } from '../../setup/auth';
 
 export function useGetSomeDocs(numberOfItems, bdKey) {
-    return useInfiniteQuery ({
+    return useQuery({
         queryKey: [bdKey, numberOfItems],
-        queryFn: async ({pageParam = 1}) => {
+        queryFn: async () => {
             const collectionRef = collection(db, bdKey);
             let q;
             if(bdKey === 'posts') {
-                q = query(collectionRef, limit(numberOfItems), orderBy('date', 'desc'), startAfter(pageParam))
+                q = query(collectionRef, limit(numberOfItems), orderBy('date', 'desc'))
             }
             else {
-                q = query(collectionRef, limit(numberOfItems), startAfter(pageParam))
+                q = query(collectionRef, limit(numberOfItems))
             }
             const querySnapshot = await getDocs(q);
             const users = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-            return {users, nextPageParam: users.length > 0 ? pageParam + 1 : undefined};
+            return users;
         },
-        getNextPageParam: (lastPage, allPages) => lastPage.nextPageParam,
         placeholderData: keepPreviousData,
     });
 }
