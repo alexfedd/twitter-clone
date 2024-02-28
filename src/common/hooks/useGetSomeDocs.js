669955539672f1438227687625a96e0f1597ba58
alexfedd <alexandrfedorov508@gmail.com>
2024-02-28 19:35:37 +0300

    Bad

--- src/common/hooks/useGetSomeDocs.js
+++ src/common/hooks/useGetSomeDocs.js
@@ -1,23 +1,24 @@
-import { useQuery, keepPreviousData } from '@tanstack/react-query';
-import { collection, getDocs, limit, orderBy, query } from 'firebase/firestore';
+import { keepPreviousData, useInfiniteQuery  } from '@tanstack/react-query';
+import { collection, getDocs, limit, orderBy, query, startAfter } from 'firebase/firestore';
 import { db } from '../../setup/auth';
 
 export function useGetSomeDocs(numberOfItems, bdKey) {
-    return useQuery({
+    return useInfiniteQuery ({
         queryKey: [bdKey, numberOfItems],
-        queryFn: async () => {
+        queryFn: async ({pageParam = 1}) => {
             const collectionRef = collection(db, bdKey);
             let q;
             if(bdKey === 'posts') {
-                q = query(collectionRef, limit(numberOfItems), orderBy('date', 'desc'))
+                q = query(collectionRef, limit(numberOfItems), orderBy('date', 'desc'), startAfter(pageParam))
             }
             else {
-                q = query(collectionRef, limit(numberOfItems))
+                q = query(collectionRef, limit(numberOfItems), startAfter(pageParam))
             }
             const querySnapshot = await getDocs(q);
             const users = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-            return users;
+            return {users, nextPageParam: users.length > 0 ? pageParam + 1 : undefined};
         },
+        getNextPageParam: (lastPage, allPages) => lastPage.nextPageParam,
         placeholderData: keepPreviousData,
     });
 }
